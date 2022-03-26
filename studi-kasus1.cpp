c9c693e0b6a8d217e8fec1056c08ba84b9fa1a2f
ilhamfauzan630 <fauzanilham630@gmail.com>
2022-03-26 14:58:57 +0700

    kerangka kode studi kasus

---
+++ studi-kasus1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
+
+class bensin{
+	public:
+		void pertalite(liter){
+			
+		}
+		void pertalite(liter){
+			
+		}
+};
+
+int main(){
+	
+	
+	getch();
+	return 0;
+}
