60725571b727b00533ab725a6777522ec666f064
ilhamfauzan630 <fauzanilham630@gmail.com>
2022-03-26 15:23:30 +0700

    update by drea = menambahkan switch case dan beberapa pilihan sederhana

--- studi-kasus1.cpp
+++ studi-kasus1.cpp
@@ -5,15 +5,52 @@ using namespace std;
 
 class bensin{
 	public:
-		void pertalite(liter){
+		void pertalite(int liter){
 			
 		}
-		void pertalite(liter){
+		void pertalite(int liter){
 			
 		}
 };
 
+void struk(){
+	ofstream myfile;
+	myfile open(SPBU.txt)
+	
+	
+	myfile.close;
+}
+
 int main(){
+	string nama;
+	float liter;
+	
+	cout << "============================"
+	cout << "Program SPBU"<<endl;
+	cout << "Tuliskan nama : ";
+	getline(cin,nama);
+	cout << "pilih Bensin : "<<endl;
+	cout << "1. Pertalite"<<endl;
+	cout << "2. Pertamax"<<endl;
+	cout << "input :";
+	cin << bensin1;
+	
+	cout << "Liter : ";
+	cin >>liter;
+	
+	switch (bensin1){
+		case 1:
+			pertalite(liter);
+			break;
+		case 2:
+			pertamax(liter);
+			break;
+			
+		default:
+			cout << "silakan inputkan dengan benar"<<endl;
+			cout << "keluar program";
+			break;
+	}
 	
 	
 	getch();
