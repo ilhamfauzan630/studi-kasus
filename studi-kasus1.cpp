20d07e922f2869cec826bf1ba1ee5e27191ceb50
ilhamfauzan630 <fauzanilham630@gmail.com>
2022-03-26 16:15:21 +0700

    update by zaki = menambahkan struk dan diskusi untuk fix bug pada program

--- studi-kasus1.cpp
+++ studi-kasus1.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 #include <conio.h>
+#include <fstream>
 using namespace std;
 
 class bensin{
 	public:
-		void pertalite(float liter){
+		void pertalite(float liter,string nama){
 			int harga;
 			
 			harga = liter * 7000;
@@ -13,8 +14,26 @@ class bensin{
 			cout << "Total liter : "<<liter<<endl;
 			cout << "Jenis bensin : Pertalite"<<endl;
 			cout << "Total harga : RP "<<harga<<endl;
+			cout << "Nama pelanggan : "<<nama <<endl;
+			cout << "=================terima kasih================"<<endl;
+			cout << "============Silakan datang kembali===========";
+			
+			ofstream myfile;
+			myfile.open("SPBU.txt");
+	
+			myfile << "==============STRUK BENSIN===================="<<endl;
+			myfile << "Total liter : "<<liter<<endl;
+			myfile << "Jenis bensin : Pertalite"<<endl;
+			myfile << "Total harga : RP "<<harga<<endl;
+			myfile << "Nama pelanggan : "<<nama <<endl;
+			myfile << "=================terima kasih================"<<endl;
+			myfile << "============Silakan datang kembali===========";
+			
+			myfile.close();
+		
 		}
-		void pertamax(float liter){
+		
+		void pertamax(float liter, string nama){
 			int harga;
 			
 			harga = liter * 9000;
@@ -23,6 +42,22 @@ class bensin{
 			cout << "Total liter : "<<liter<<endl;
 			cout << "Jenis bensin : Pertamax"<<endl;
 			cout << "Total harga : "<<harga<<endl;
+			cout << "Nama pelanggan : "<<nama<<endl;
+			cout << "=================terima kasih================"<<endl;
+			cout << "============Silakan datang kembali===========";
+			
+			ofstream myfile;
+			myfile.open("SPBU.txt");
+	
+			myfile << "==============STRUK BENSIN===================="<<endl;
+			myfile << "Total liter : "<<liter<<endl;
+			myfile << "Jenis bensin : Pertalite"<<endl;
+			myfile << "Total harga : RP "<<harga<<endl;
+			myfile << "Nama pelanggan : "<<nama<<endl;
+			myfile << "=================terima kasih================"<<endl;
+			myfile << "============Silakan datang kembali===========";
+			
+			myfile.close();
 		}
 };
 
@@ -48,22 +83,18 @@ int main(){
 	
 	switch (bensin1){
 		case 1:
-			bsn.pertalite(liter);
-			cout << "Nama Pelanggan : "<<nama<<endl;
-			cout << "=================terima kasih================"<<endl;
-			cout << "============Silakan datang kembali===========";
+			bsn.pertalite(liter,nama);
+			
 			break;
+			
 		case 2:
-			bsn.pertamax(liter);
-			cout << "Nama Pelanggan : "<<nama<<endl;
-			cout << "=================terima kasih================"<<endl;
-			cout << "============Silakan datang kembali===========";
+			bsn.pertamax(liter,nama);
+			
 			break;
 			
 		default:
 			cout << "silakan inputkan dengan benar"<<endl;
 			cout << "keluar program";
-			
 			break;
 	}
 	
